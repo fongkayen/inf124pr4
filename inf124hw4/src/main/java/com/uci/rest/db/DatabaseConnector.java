f21e512fb8d58065d5f66004d12630e69d861312
Unknown <kaitlynf@uci.edu>
2018-06-04 20:26:56 -0700

    timezone

--- inf124hw4/src/main/java/com/uci/rest/db/DatabaseConnector.java
+++ inf124hw4/src/main/java/com/uci/rest/db/DatabaseConnector.java
@@ -17,12 +17,14 @@ public class DatabaseConnector {
     public static Connection getConnection() {
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         try {
-            return DriverManager.getConnection("jdbc:mysql://" + getHost() + "/" + getDatabaseName(),
+            return DriverManager.getConnection("jdbc:mysql://" + getHost() + "/" + getDatabaseName() + "?useSSL=FALSE&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
                     getUser(), getPassword());
         } catch (SQLException e) {
             System.out.println(e.getMessage());
